cfd7499640285dd048abd54cd4091537985f6270
bkleinen <bkleinen@users.noreply.github.com>
2023-07-25 15:26:43 +0000

    deploy: htw-imi-showtime/showtime-website@fb421cc0d651fd04819738bdcd6314ba85fa1dc8

--- js/semester-select.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", () =>
-    document.querySelector("#semester-select")?.addEventListener("change", changeSemester)
-);
-
-function changeSemester(event) {
-    window.location.href = `${event.target.value}`;
-}
